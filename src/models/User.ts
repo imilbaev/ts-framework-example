ed41995498b9d1cd7112fc845b53aa308a570d1a
imilbaev <mail@imilbaev.ru>
2019-11-11 23:16:54 +0500

    Eventing model extracted

--- src/models/User.ts
+++ src/models/User.ts
@@ -6,11 +6,7 @@ interface UserProps {
   age?: number;
 }
 
-type Callback = () => void;
-
 export class User {
-  events: { [key: string]: Callback[] } = {};
-
   constructor(private data: UserProps) {}
 
   get(propName: string): (string | number) {
@@ -21,28 +17,20 @@ export class User {
     Object.assign(this.data, update);
   }
 
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
-
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-
-    if (!handlers || handlers.lenth === 0) {
-      return;
-    }
-
-    handlers.forEach(callback =>{
-      callback();
-    })
-  }
-
   fetch(): void {
     axios.get(`http://localhost:3000/users/${this.get('id')}`)
     .then(( response: AxiosResponse ) => {
       this.set(response.data);
     });
+  }
+
+  save(): void {
+    const id = this.get('id');
+
+    if (this.get("id")) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      axios.post('http://locahost:3000/users', this.data);
+    }
   }
 }
